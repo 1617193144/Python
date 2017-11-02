530f53b76bc38f20e456ff012f1d8906ea198422
1617193144 <757556644@qq.com>
2017-11-02 13:29:33 +0800

    test

--- 用Python发送消息.py
+++ 用Python发送消息.py
@@ -3,21 +3,21 @@ import email.mime.multipart
 import email.mime.text
 
 msg = email.mime.multipart.MIMEMultipart()
-msg['from'] = 'tonyemail@126.com'
+msg['from'] = 'DS757556644@126.com'
 msg['to'] = '757556644@qq.com'
 msg['subject'] = 'test'
 content = '''''
-     你好，
-             这是一封自动发送的邮件。
+    你好，
+            这是一封自动发送的邮件。
 
 
- '''
+'''
 txt = email.mime.text.MIMEText(content)
 msg.attach(txt)
 
 smtp = smtplib
 smtp = smtplib.SMTP()
 smtp.connect('smtp.126.com', '25')
-smtp.login('tonyemail@126.com', '*******')
-smtp.sendmail('tonyemail@126.com', '358777330@qq.com', str(msg))
+smtp.login('DS757556644@126.com', 'ZXCVBNM121299')
+smtp.sendmail('DS757556644@126.com', '757556644@qq.com', str(msg))
 smtp.quit()
